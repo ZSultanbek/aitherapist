da3bf7e28f793e4d46620b242781d983536ec4e6
Humpty Dumpty <zhanibek220506@gmail.com>
2025-04-16 21:09:03 +0500

    fixed homepage

--- mentalhealthai/mentalhealthai/urls.py
+++ mentalhealthai/mentalhealthai/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 from api.views import chat, register
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
@@ -28,4 +29,5 @@ urlpatterns = [
     path("register/", register, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="/login"), name="logout"),
+    path('', RedirectView.as_view(url='/home/', permanent=False)),
 ]
